0ffb05a881a7653663e68e1412ddd439c04d1ad1
eunsunshin <esshin@rencar.co.kr>
2022-07-20 18:00:50 +0900

    Add: ResetCss 추가

---
+++ src/styles/index.ts
@@ -0,0 +1,3 @@
+import ResetCss from './reset';
+
+export { ResetCss };
--- src/styles/reset.ts
+++ src/styles/reset.ts
@@ -1,1 +1,11 @@
 import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+export default createGlobalStyle`
+  ${reset}
+
+  * {
+    box-sizing: border-box;
+  }
+`;
+
